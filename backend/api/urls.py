75d5c03b680738ac726aac9af544ce90cb7c1cdd
Ксения Фурсова <kcehna@me.com>
2022-08-15 13:30:33 +0300

    Модель рецептов

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
@@ -5,14 +6,15 @@ from api.views import RecipesViewSet
 
 app_name = 'api'
 
-router_ver1 = SimpleRouter('')
-router_ver1.register(
+router_v1 = SimpleRouter('')
+# router_v1.register(r'users', AbstractUser, basename='users')
+router_v1.register(
     'recipes',
     RecipesViewSet,
     basename='recipes'
 )
 
 urlpatterns = [
-    path('v1/', include(router_ver1.urls)),
-    # path('v1/', include('djoser.urls.jwt')),
+    path('', include(router_v1.urls)),
+    # path('', include('users.urls')),
 ]
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,11 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Recipe(models.Model):
+    """Модель рецептов пользователей."""
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
     name = models.CharField(
-        'Название рецепта',
-        max_length=150)
+        'Название',
+        max_length=200
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='recipes/images/',
+    )
+    text = models.TextField('Описание')
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления',
+    )
+
+    class Meta:
+        ordering = ('-pub_date', )
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
 
     def __str__(self):
         return self.name
+
     
