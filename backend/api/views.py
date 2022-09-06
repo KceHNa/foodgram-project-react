a1c4940e8e2ba038c363c35416f7c0cbdffd7014
Kseniya F <kcehna@me.com>
2022-09-06 16:36:41 +0300

    Композы и воркфлоу для пушей и инсталяций

--- backend/api/views.py
+++ backend/api/views.py
@@ -110,7 +110,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
 
     @staticmethod
     def __delete_method(model, pk, user):
-        obj = get_object_or_404(model, user=user, recipe__id=pk)
+        obj = model.objects.filter(model, user=user, recipe__id=pk)
         if obj.exists():
             obj.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
