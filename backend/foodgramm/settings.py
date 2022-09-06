5c32e06a7707b7ffac4c04d6923154e2d0453ff1
Kseniya Fursova <kcehna@me.com>
2022-09-06 20:10:40 +0300

    Update settings.py

--- backend/foodgramm/settings.py
+++ backend/foodgramm/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='mytestkey')
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
